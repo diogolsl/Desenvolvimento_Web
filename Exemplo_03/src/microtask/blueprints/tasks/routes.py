3a69c325177b285a1ade47a93ef9fd4c23cd7cb6
diogolsl <diogolucasdelucena@gmail.com>
2026-02-27 09:25:37 -0300

    .

--- Exemplo_03/src/microtask/blueprints/tasks/routes.py
+++ Exemplo_03/src/microtask/blueprints/tasks/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from . import bp
 
 @bp.route('/')
 def listar_tarefas():
-    return "Listagem de tarefas"
+    tarefas = [ "Estudar Flask", "Criar API", "Testar aplicação" ]
+    return render_template('tasks/listar_tarefas.html', tarefas=tarefas)
